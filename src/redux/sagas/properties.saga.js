5aebd86175f871922acf98cba873702f1221fa54
Emma Molden <emma.molden007@gmail.com>
2024-09-30 12:49:37 -0500

    added two functions to the propertiesSaga

--- src/redux/sagas/properties.saga.js
+++ src/redux/sagas/properties.saga.js
@@ -13,12 +13,19 @@ function* deleteProperty(action) {
 function* updateProperty(action) {
 }
 
+function* backToDefault(action) {
+}
+
+function* getPropertyOfInterest(action) {
+}
 
 function* propertiesSaga() {
-    yield takeLatest('GET_PROPERTY', getProperties);
+    yield takeLatest('GET_PROPERTIES', getProperties);
     yield takeLatest('ADD_PROPERTY', addProperty);
     yield takeLatest('DELETE_PROPERTY', deleteProperty);
     yield takeLatest('UPDATE_PROPERTY', updateProperty);
+    yield takeLatest('BACK_TO_DEFAULT', backToDefault);
+    yield takeLatest('GET_PROPERTY_OF_INTEREST', getPropertyOfInterest);
   }
   
   export default propertiesSaga;
