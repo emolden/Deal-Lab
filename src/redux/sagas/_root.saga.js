aba0da9f8ee6e5173ea7a76e812c5a7397e76bbe
Emma Molden <emma.molden007@gmail.com>
2024-09-30 13:04:25 -0500

    added defaultSettingsSaga

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -5,6 +5,7 @@ import userSaga from './user.saga';
 import propertiesSaga from './properties.saga';
 import propertyHoldingItemsSaga from './propertyHoldingItems.saga';
 import propertyRepairItemsSaga from './PropertyRepairItems.saga';
+import defaultSettingsSaga from './defaultSettings.saga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -21,5 +22,6 @@ export default function* rootSaga() {
     propertiesSaga(),
     propertyHoldingItemsSaga(),
     propertyRepairItemsSaga(),
+    defaultSettingsSaga(),
   ]);
 }
