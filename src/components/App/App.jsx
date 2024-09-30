c757c3d4edd0fb4e1dca48071c6de431120064cc
Emma Molden <emma.molden007@gmail.com>
2024-09-30 10:56:34 -0500

    changed LandingPage to PropertyPage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -16,7 +16,7 @@ import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 import AboutPage from '../AboutPage/AboutPage';
 import UserPage from '../UserPage/UserPage';
 import InfoPage from '../InfoPage/InfoPage';
-import LandingPage from '../LandingPage/LandingPage';
+import PropertyPage from '../PropertyPage/PropertyPage';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
 
@@ -105,8 +105,8 @@ function App() {
               // redirect them to the /user page
               <Redirect to="/user" />
               :
-              // Otherwise, show the Landing page
-              <LandingPage />
+              // Otherwise, show the Property page
+              <PropertyPage />
             }
           </Route>
 
