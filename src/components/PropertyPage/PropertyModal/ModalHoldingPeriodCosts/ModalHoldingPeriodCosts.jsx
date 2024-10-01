46262dc55167632618f6de2d1c0ccf3929dc7440
Emma Molden <emma.molden007@gmail.com>
2024-10-01 11:41:08 -0500

    holding items rendering on MOdalHoldingPeriodCosts component

--- src/components/PropertyPage/PropertyModal/ModalHoldingPeriodCosts/ModalHoldingPeriodCosts.jsx
+++ src/components/PropertyPage/PropertyModal/ModalHoldingPeriodCosts/ModalHoldingPeriodCosts.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
+
 
 function ModalHoldingPeriodCosts() {
+  const propertyOfInterest = useSelector((store) => store.propertyOfInterest);
+
   return (
     <div className="container">
-      <p>Holding Period Costs:</p>
+        {Object.keys(propertyOfInterest).length && 
+      <>
+      <p>Holding Costs:</p>
+      <p>Holding Items:</p>
+      <ul>
+        <li> Taxes: ${propertyOfInterest.property[0].taxes_yearly/12}</li>
+        {propertyOfInterest.holdingItems.map((item) => {
+          return (
+            <li key = {item.holding_items_id}>{item.holding_name}: ${item.holding_cost} </li>
+          )
+        })}
+      </ul>
+      <p>Holding Period: {propertyOfInterest.property[0].holding_period} Months</p>
+      
+      </>
+      }
     </div>
   );
 }
