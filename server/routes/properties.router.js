44806b0eced7b516111b1003330704024b69668b
Emma Molden <emma.molden007@gmail.com>
2024-10-01 10:58:33 -0500

    /api/properties/propertyOfInterest/id successfully retrieves holdingItem data from database

--- server/routes/properties.router.js
+++ server/routes/properties.router.js
@@ -74,9 +74,24 @@ router.get('/propertyOfInterest/:id', rejectUnauthenticated, async (req, res) =>
     `;
     const repairItemValue = [propertyId]
     const repairItemResult = await connection.query(repairItemText, repairItemValue);
-    console.log('database reponse to repairItem: ', repairItemResult.rows)
+    // console.log('database reponse to repairItem: ', repairItemResult.rows)
 
-    //requests holding items for specific property
+   
+      //request holding items for specific property
+      const holdingItemText = `
+        SELECT 
+          "holding_items"."id" AS "holding_items_id",
+          "properties"."id" AS "property_id",
+          "holding_items"."name" AS "holding_name",
+          "holding_items"."cost" AS "holding_cost" 
+          FROM "properties"
+          JOIN "holding_items"
+            ON "properties"."id" = "holding_items"."property_id"
+          WHERE "properties"."id" = $1;
+    `;
+    const holdingItemValue = [propertyId]
+    const holdingItemResult = await connection.query(holdingItemText, holdingItemValue);
+    console.log('database reponse to holdingItem: ', holdingItemResult.rows)
 
 
 
