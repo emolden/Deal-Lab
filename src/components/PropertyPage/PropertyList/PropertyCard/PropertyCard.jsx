235bb645eab1290c7ece759f0102aa708255d6d7
Emma Molden <emma.molden007@gmail.com>
2024-10-01 08:57:24 -0500

    created an edit button in the PropertyCard component

--- src/components/PropertyPage/PropertyList/PropertyCard/PropertyCard.jsx
+++ src/components/PropertyPage/PropertyList/PropertyCard/PropertyCard.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 function PropertyCard() {
+
+
+  
+
   return (
     <div className="container">
       <p>Property Card:</p>
+      <button onClick={getPropertyOfInterest}>Edit</button>
     </div>
   );
 }
