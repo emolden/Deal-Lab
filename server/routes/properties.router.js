7e6d704692baf47c7c9d94944204196245152b89
Emma Molden <emma.molden007@gmail.com>
2024-10-01 10:55:37 -0500

    /api/properties/propertyOfInterest/id successfully retrieves repairItem data from database

--- server/routes/properties.router.js
+++ server/routes/properties.router.js
@@ -48,6 +48,7 @@ router.get('/propertyOfInterest/:id', rejectUnauthenticated, async (req, res) =>
     await connection.query('BEGIN;')
 
     const propertyId = req.params.id;
+    
 
     //requests specific property info from the properties table
     const propertyText = `
@@ -56,8 +57,38 @@ router.get('/propertyOfInterest/:id', rejectUnauthenticated, async (req, res) =>
     `;
     const propertyValue = [propertyId]
     const propertyResult = await connection.query(propertyText, propertyValue);
-    console.log('database reponse to property: ', propertyResult.rows)
+    // console.log('database reponse to property: ', propertyResult.rows)
 
+
+    //request repair items for specific property
+    const repairItemText = `
+      SELECT 
+        "repair_items"."id" AS "repair_items_id",
+        "properties"."id" AS "property_id",
+        "repair_items"."name" AS "repair_name",
+        "repair_items"."cost" AS "repair_cost" 
+        FROM "properties"
+        JOIN "repair_items"
+          ON "properties"."id" = "repair_items"."property_id"
+        WHERE "properties"."id" = $1;
+    `;
+    const repairItemValue = [propertyId]
+    const repairItemResult = await connection.query(repairItemText, repairItemValue);
+    console.log('database reponse to repairItem: ', repairItemResult.rows)
+
+    //requests holding items for specific property
+
+
+
+    await connection.query('COMMIT;')
+
+    // const propertyData = {
+    //   property : propertyResult.rows,
+    //   repairItems : ,
+    //   holdingItems : 
+    // }
+
+    // res.send(response)
   } catch (error) {
     console.log('error in /api/properties/propertyOfInterest/id GET route: ', error)
     await connection.query('ROLLBACK;')
