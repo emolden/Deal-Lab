254944017a4d895e4fa18b5ce9289f6629ab3665
Emma Molden <emma.molden007@gmail.com>
2024-10-01 09:07:48 -0500

    onClick function sends GET PROPERTY OF INTEREST dispatch

--- src/components/PropertyPage/PropertyList/PropertyCard/PropertyCard.jsx
+++ src/components/PropertyPage/PropertyList/PropertyCard/PropertyCard.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 function PropertyCard() {
+  const dispatch = useDispatch();
 
+  const getPropertyOfInterest = (propId) => {
+    console.log("in getPropertyOfInterest function in PropertyCard component", propId)
+    dispatch({
+      type: 'GET_PROPERTY_OF_INTEREST',
+      payload: propId
+    });
+  }
 
-  
 
   return (
     <div className="container">
       <p>Property Card:</p>
-      <button onClick={getPropertyOfInterest}>Edit</button>
+      <button onClick={()=>{getPropertyOfInterest(1)}}>Edit</button>
     </div>
   );
 }
