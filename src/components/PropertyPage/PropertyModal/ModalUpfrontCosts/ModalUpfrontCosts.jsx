50d085c334456b6539cf3dce7e69da613a3bc390
Emma Molden <emma.molden007@gmail.com>
2024-10-03 14:58:00 -0500

    repair item name and cost inputs in the modal

--- src/components/PropertyPage/PropertyModal/ModalUpfrontCosts/ModalUpfrontCosts.jsx
+++ src/components/PropertyPage/PropertyModal/ModalUpfrontCosts/ModalUpfrontCosts.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
+import { useState } from 'react';
 
 function ModalUpfrontCosts() {
   const propertyOfInterest = useSelector((store) => store.propertyOfInterest);
+  const [repairName, setRepairName] = useState("");
+  const [repairCost, setRepairCost] = useState("");
 
   return (
     <div className="container">
@@ -11,6 +14,21 @@ function ModalUpfrontCosts() {
       <p>Upfront Costs:</p>
       <p> Purchase Price: {propertyOfInterest.property[0].purchase_price}</p>
       <p>Repair Items:</p>
+      <input className='repiarItemInput'
+        name='repairItemInput'
+        type='text'
+        placeholder='Repair Name'
+        value={repairName}
+        onChange={e => setRepairName(e.target.value)}
+      />
+      <input className='repiarCostInput'
+        name='repairCostInput'
+        type='text'
+        placeholder='Repair Cost'
+        value={repairCost}
+        onChange={e => setRepairCost(e.target.value)}
+      />
+      <button>Add</button>
       <ul>
         {propertyOfInterest.repairItems.map((item) => {
           return (
