2c399e5320da2b84de14a6e8b83dddcab23392f9
Emma Molden <emma.molden007@gmail.com>
2024-10-09 13:29:03 -0500

    resolved more merge conflicts

--- src/components/PropertyPage/PropertyModal/ModalUpfrontCosts/ModalUpfrontCosts.jsx
+++ src/components/PropertyPage/PropertyModal/ModalUpfrontCosts/ModalUpfrontCosts.jsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import { useState } from 'react';
-<<<<<<< HEAD
-
-
-=======
-import upfrontCost from '../../../../helpers/upfrontCost';
-import repairCost from '../../../../helpers/repairCost';
-// import '../PropertyPage/PropertyModal/PropertyModal.css';
->>>>>>> main
 
 function ModalUpfrontCosts() {
 
