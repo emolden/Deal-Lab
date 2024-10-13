71f3c582571a93db923c04707299c21ac36f5cd1
Emma Molden <emma.molden007@gmail.com>
2024-10-13 13:42:33 -0500

    updated interestRateAnnual in the motgage calculator router

--- server/routes/mortgageCalculator.router.js
+++ server/routes/mortgageCalculator.router.js
@@ -100,7 +100,7 @@ router.post('/:id', async (req, res) => {
             // console.log('totalInterestPaid:', totalInterestPaid);
             
             // *************************SHOULD THE PURCHASE PRICE BE THE LOAN AMOUNT????? ******************************
-            interestRateAnnual = Number(((((totalInterestPaid / purchasePrice) / (defaultLoanTerm * 365)) * 365) * 100).toFixed(3));
+            interestRateAnnual = Number(((((totalInterestPaid / baseLoanAmount) / (defaultLoanTerm * 365)) * 365) * 100).toFixed(3));
             // console.log('interestRateAnnual', interestRateAnnual);
             
 
