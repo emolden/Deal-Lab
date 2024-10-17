b6a973fd1dbf9ec671b0e160f7bc41f7cfeccc98
Emma Molden <emma.molden007@gmail.com>
2024-10-17 15:23:27 -0500

    added comment

--- src/components/PropertyPage/PropertyModal/ModalHoldingPeriodCosts/ModalHoldingPeriodCosts.jsx
+++ src/components/PropertyPage/PropertyModal/ModalHoldingPeriodCosts/ModalHoldingPeriodCosts.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import { useState } from 'react';
 
-
+//property page
 function ModalHoldingPeriodCosts() {
 
   const dispatch = useDispatch();
